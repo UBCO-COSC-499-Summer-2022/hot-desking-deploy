e89382e55b1fc84e719ecf52298f1fc257d07e47
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2022-07-30 14:43:45 -0700

    Updated bash script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cp docker-compose.mac.yml docker-compose.yml
 cd src/
+cp .env.example .env
 docker-compose up -d --build site
 docker-compose run --rm composer update
 docker-compose run --rm npm install
@@ -8,3 +10,4 @@ docker-compose run --rm npm run prod
 docker-compose run --rm artisan migrate:refresh
 docker-compose run --rm artisan db:seed --class=UserSeeder
 docker-compose run --rm artisan db:seed --class=ResourceSeeder
+
