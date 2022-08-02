f9499e6cbcf0a9fd10f81b70019336ccd0bcc7c0
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2022-08-01 17:52:06 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ cp .env.example .env
 ls
 cd ..
 ls
-docker compose up -d --build site
-docker compose run --rm composer update
-docker compose run --rm npm install
-docker compose run --rm npm run dev
-docker compose run --rm artisan migrate:refresh
-docker compose run --rm artisan db:seed --class=ResourceSeeder
-docker compose run --rm artisan db:seed --class=UserSeeder
+docker-compose up -d --build site
+docker-compose run --rm composer update
+docker-compose run --rm npm install
+docker-compose run --rm npm run dev
+docker-compose run --rm artisan migrate:refresh
+docker-compose run --rm artisan db:seed --class=ResourceSeeder
+docker-compose run --rm artisan db:seed --class=UserSeeder
