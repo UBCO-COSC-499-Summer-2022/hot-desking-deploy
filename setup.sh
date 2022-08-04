c08a0875e4861dd5fc31d81e39860567ebac0687
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2022-08-03 23:06:39 -0700

    give permission

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-cp docker-compose.mac.yml docker-compose.yml
-ls
-cd src/
-cp .env.production .env
-ls
-cd ..
-ls
-docker compose down
-docker compose up -d --build site
-docker compose run --rm composer update
-docker compose run --rm npm install
-docker compose run --rm npm run prod
-docker compose run --rm artisan migrate
-echo DONE
+chown -R www-data: hot-desking-test
+
+# cp docker-compose.mac.yml docker-compose.yml
+# ls
+# cd src/
+# cp .env.production .env
+# ls
+# cd ..
+# ls
+# docker compose down
+# docker compose up -d --build site
+# docker compose run --rm composer update
+# docker compose run --rm npm install
+# docker compose run --rm npm run prod
+# docker compose run --rm artisan migrate
+# echo DONE
 
 # docker compose run --rm artisan db:seed --class=ResourceSeeder
 # docker compose run --rm artisan db:seed --class=UserSeeder
