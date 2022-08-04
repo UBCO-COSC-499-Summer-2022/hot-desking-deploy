7c78b6149e2d6b4c44e83819b2e7679df96d81e4
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2022-08-03 23:33:29 -0700

    turn down site

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ docker compose run --rm composer update
 docker compose run --rm npm install
 docker compose run --rm npm run prod
 docker compose run --rm artisan migrate
+docker compose down
 echo DONE
 
 # docker compose run --rm artisan db:seed --class=ResourceSeeder
