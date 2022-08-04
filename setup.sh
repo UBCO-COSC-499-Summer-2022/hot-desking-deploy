11040d5e9d96ddc57d987d507c60ac765fe1896f
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2022-08-03 23:11:04 -0700

    update

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-chown -R www-data: hot-desking-test
+chown -R www-data: ../hot-desking-test1
 
-# cp docker-compose.mac.yml docker-compose.yml
-# ls
-# cd src/
-# cp .env.production .env
-# ls
-# cd ..
-# ls
-# docker compose down
-# docker compose up -d --build site
-# docker compose run --rm composer update
-# docker compose run --rm npm install
-# docker compose run --rm npm run prod
-# docker compose run --rm artisan migrate
-# echo DONE
+cp docker-compose.mac.yml docker-compose.yml
+ls
+cd src/
+cp .env.production .env
+ls
+cd ..
+ls
+docker compose down
+docker compose up -d --build site
+docker compose run --rm composer update
+docker compose run --rm npm install
+docker compose run --rm npm run prod
+docker compose run --rm artisan migrate
+echo DONE
 
 # docker compose run --rm artisan db:seed --class=ResourceSeeder
 # docker compose run --rm artisan db:seed --class=UserSeeder
