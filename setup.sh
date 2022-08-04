d40d0caedbd176ed30ed94c1b725e1c7dcf6326c
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2022-08-03 22:38:17 -0700

    update

--- setup.sh
+++ setup.sh
@@ -12,7 +12,8 @@ docker compose up -d --build site
 docker compose run --rm composer update
 docker compose run --rm npm install
 docker compose run --rm npm run prod
+docker compose run --rm artisan migrate
 echo DONE
-# docker compose run --rm artisan migrate:refresh
+
 # docker compose run --rm artisan db:seed --class=ResourceSeeder
 # docker compose run --rm artisan db:seed --class=UserSeeder
