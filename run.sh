aa5929b6080693fde489318c21ed556fd7a83a61
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2022-08-08 01:31:28 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,10 +6,7 @@ docker compose run --rm composer update
 docker compose run --rm npm install
 docker compose run --rm npm run prod
 docker compose run --rm artisan migrate --force
-# # clear schedule cahce
-# docker compose run --rm artisan cache:clear
-# docker compose run --rm artisan queue:restart
-# docker compose run --rm schedule:clear-cache
 # add a single cron configuration entry to our server that runs the schedule:run command every minute.
-* * * * * docker compose run --rm artisan schedule:run >> /dev/null 2>&1
+cd .. 
+* * * * * cd test-pull5/ && php artisan schedule:run >> /dev/null 2>&1
 echo Server is LIVE
