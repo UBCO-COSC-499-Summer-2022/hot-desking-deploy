538500b37a27add2dbeffc9606c2ef9aa1401f5e
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2022-08-08 00:56:08 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,8 @@ docker compose run --rm npm run prod
 # TODO REMOVE FORCE FLAG
 docker compose run --rm artisan migrate --force
 # clear schedule cahce
+docker compose run --rm artisan cache:clear
+docker compose run --rm artisan queue:restart
 docker compose run --rm schedule:clear-cache
 # add a single cron configuration entry to our server that runs the schedule:run command every minute.
 * * * * * docker compose run --rm artisan schedule:run >> /dev/null 2>&1
