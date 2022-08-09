369b5f1236e62d2305453f2f9a7f0621016e9928
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2022-08-09 12:24:54 -0700

    update .drone and bash files

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ docker compose run --rm composer update
 docker compose run --rm npm install
 docker compose run --rm npm run prod
 docker compose run --rm artisan migrate --force
-echo Server is LIVE
+docker compose up -d --build cron
